35d3e9b4f378ad3f409b52a1d70b0733acb32201
German Kravtsov <grk55rus@mail.ru>
2023-10-11 23:44:45 +0600

    add a good proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React, {useState, useEffect} from "react"
 import Axios from "axios"
-import './App.css'
 
 const App = () => {
   const [data, setData] = useState("")
 
   const getData = async() => {
-    const response = await Axios.get("http://localhost:3000/api")
+    const response = await Axios.get("http://localhost:5173/api/getData")
     setData(response.data);
   }
 
